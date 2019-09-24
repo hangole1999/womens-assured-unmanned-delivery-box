a6574f92d4db23fc4cdba97aebffba33baab1cca
Gi-Hwang Kim <hangole1999@gmail.com>
2019-09-25 00:30:50 +0900

    [Auto] Fix many

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 git pull origin master
 npm install
 npm run build
-sudo rm -rf /var/www/womens-assured-unmanned-delivery-box/*
-sudo cp -r dist/* /var/www/womens-assured-unmanned-delivery-box
+sudo rm -rf /var/www/html/*
+sudo cp -r dist/* /var/www/html
